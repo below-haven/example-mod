4201123330de1431e5a755c2f5ac85e821e6b6e5
ponei <github@ponei.eu>
2026-06-28 15:35:34 -0300

    chore: update mappings

--- src/main/java/com/bh/testmod/mixin/BlockingCommunicatorMixin.java
+++ src/main/java/com/bh/testmod/mixin/BlockingCommunicatorMixin.java
@@ -9,14 +9,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BlockingCommunicator.class)
 public abstract class BlockingCommunicatorMixin {
-    @Inject(method = "method_11857", at = @At("HEAD"))
+    @Inject(method = "sendMessage", at = @At("HEAD"))
     protected void sendMessage(@Coerce Object arg, CallbackInfo ci) {
         System.out.println("[bh] sendMessage");
         System.out.println("arg = " + arg);
         System.out.println("arg class = " + (arg == null ? "null" : arg.getClass().getName()));
     }
 
-    @Inject(method = "method_11858", at = @At("HEAD"))
+    @Inject(method = "sendDatagram", at = @At("HEAD"))
     protected void sendDatagram(@Coerce Object arg, CallbackInfo ci) {
         System.out.println("[bh] sendDatagram");
         System.out.println("arg = " + arg);
